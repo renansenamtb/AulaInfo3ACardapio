3e40f57bbe99dd5c53d5b9323cf5300b54c124c9
horse <renansenazzz@gmail.com>
2024-07-29 21:15:06 -0300

    ta pronto o sorvetinho
    vdd

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,83 @@
 import { useState } from "react";
 import "../src/globals.css";
 
-
 export default function App() {
-const [listaProdutos, setProdutos] = useState([
-
+    const [listaProdutos, setProdutos] = useState([
         {
-
             id: 1,
-
             item: "Hambúrguer",
-
             imagem: "https://www.assai.com.br/sites/default/files/shutterstock_1806472312.jpg",
-
             preco: "R$ 25,99"
-
         },
-
         {
-
             id: 2,
-
             item: "Coca-cola 350ml",
-
             imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm4S15squn95k7qtrVOpMX1MOJGe48y4B7FQ&s",
-
             preco: "R$ 5,99"
-
         },
-
         {
-
             id: 3,
-
             item: "Batatas fritas",
-
             imagem: "https://gastronomiacarioca.zonasul.com.br/wp-content/uploads/2023/05/batata_frita_destaque_ilustrativo_zona_sul.jpg",
-
             preco: "R$ 8,99"
-
         },
-
         {
-
             id: 4,
-
             item: "Suco de Frutas",
-
             imagem: "https://helenalunardelli.com.br/wp-content/uploads/2013/02/sucos.jpg",
-
             preco: "R$ 8,99"
-
         },
-
     ]);
-   
-const [listaPedidos, setPedidos] = useState([]);
-const adicionarProdutoPedido = (produto) => {
- setPedidos([...listaPedidos, produto]);
-}
 
+    const [listaPedidos, setPedidos] = useState([]);
 
-return (
-    <div className="bloco-principal">
-        <div className="bloco-produtos">
-            {
-            listaProdutos.map((produto)>
-            <div key={produto.id}>
-                <img src={produto.imagem}>
-                <p>{produto.item}</p>
-            <button onClick={() => adicionarProdutoPedido(produto)}>mim de</button>
+    const adicionarProdutoPedido = (produto) => {
+        setPedidos([...listaPedidos, produto]);
+    };
+
+    const removerProdutoPedido = (id) => {
+        setPedidos(listaPedidos.filter(produto => produto.id !== id));
+    };
+
+    return (
+        <div className="bloco-principal">
+            <div className="bloco-produtos">
+                {listaProdutos.map((produto) => (
+                    <div key={produto.id}>
+                        <img src={produto.imagem} alt={produto.item} />
+                        <p>{produto.item}</p>
+                        <p>{produto.preco}</p>
+                        <button onClick={() => adicionarProdutoPedido(produto)}>Adicionar</button>
+                    </div>
+                ))}
             </div>
-        
-        
-            )
+            <div className="bloco-pedidos">
+                <p>Meus Pedidos</p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Item</th>
+                            <th>Imagem</th>
+                            <th>Preço</th>
+                            <th>Ação</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listaPedidos.map((produto) => (
+                            <tr key={produto.id}>
+                                <td>{produto.id}</td>
+                                <td>{produto.item}</td>
+                                <td><img src={produto.imagem} alt={produto.item} style={{ width: "50px" }} /></td>
+                                <td>{produto.preco}</td>
+                                <td>
+                                    <button onClick={() => removerProdutoPedido(produto.id)}>Remover</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
-<div className="bloco-pedidos">
-    <p>Meus Pedidos</p>
-    {
-        listaPedidos.map((produto)=>
-        <div key={produto.id}>
-            <p>{produto.item} </p>
-    
-</div>
-        )
-    }
-
-    </div>
-</div>
-)
-};
